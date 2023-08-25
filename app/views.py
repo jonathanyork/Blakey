6bdb43e4d7aaa381667e456d134dd0f306bc36fc
Jonathan York <jonathan.york@gmail.com>
2023-08-25 19:17:45 -0400

    added navigation and refactored

--- app/views.py
+++ app/views.py
@@ -1,12 +1,19 @@
-import re
-from datetime import datetime
-from flask import render_template
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+from datetime import datetime
+from . import app
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
